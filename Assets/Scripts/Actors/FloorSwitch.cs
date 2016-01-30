81dcdd32aa587c2dbd10b0e1cc67791317aa4032
Mark Velthuis <da_heayray@hotmail.com>
2016-01-30 12:25:54 +0100

    Add moar logic thingies

---
+++ Assets/Scripts/Actors/FloorSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FloorSwitch : ToggleSwitch {
+
+	void OnTriggerEnter(Collider other) {
+		if (OnTriggerPress != null){
+			OnTriggerPress(this);
+		}
+    }
+
+	void OnTriggerExit(Collider other) {
+		if (OnTriggerRelease != null){
+			OnTriggerRelease(this);
+		}
+    }
+}
---
+++ Assets/Scripts/Reactors/SwitchToggled.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchToggled : MonoBehaviour {
+
+	public ToggleSwitch toggleSwitch;
+
+	// Use this for initialization
+	void Start () {
+		toggleSwitch.OnTriggerPress += HandleFloorSwitchPress;
+		toggleSwitch.OnTriggerRelease += HandleFloorSwitchRelease;
+	}
+
+	void HandleFloorSwitchPress (ToggleSwitch floorSwitch){
+		gameObject.SetActive(false);
+	}
+
+	void HandleFloorSwitchRelease (ToggleSwitch floorSwitch){
+		gameObject.SetActive(true);
+	}
+}

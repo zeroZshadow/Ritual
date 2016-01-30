ba626f50ada11eb91eaed2664a3080477716f0ff
Mark Velthuis <da_heayray@hotmail.com>
2016-01-30 22:43:40 +0100

    add selective floor trigger and load level on trigger

---
+++ Assets/Scripts/Actors/SelectiveFloorSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class SelectiveFloorSwitch : ToggleSwitch {
+
+	public List<GameObject> PossibleObjects;
+
+	void OnTriggerEnter(Collider other) {
+		if (PossibleObjects.Contains(other.gameObject)){
+			IsOn = true;
+			if (OnSwitchOn != null){
+				OnSwitchOn(this);
+			}
+		}
+    }
+
+	void OnTriggerExit(Collider other) {
+		if (PossibleObjects.Contains(other.gameObject)){
+			IsOn = false;
+			if (OnSwitchOff != null){
+				OnSwitchOff(this);
+			}
+		}
+    }
+}

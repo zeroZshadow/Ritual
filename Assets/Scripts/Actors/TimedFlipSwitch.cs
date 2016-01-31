fbeb6bc55e239ea714c4ede19aca493aa90d7aee
Mark Velthuis <da_heayray@hotmail.com>
2016-01-31 03:18:04 +0100

    add timed switch

---
+++ Assets/Scripts/Actors/TimedFlipSwitch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedFlipSwitch : ToggleSwitch {
+
+	public float resetTime;
+	private float timeTillReset;
+
+	// Update is called once per frame
+	void Update () {
+		if (IsOn){
+			if (timeTillReset < Time.time){
+				IsOn = false;
+				if (OnSwitchOff != null){
+					OnSwitchOff(this);
+				}
+			}
+		}
+	}
+
+	public override void Interact(){
+		if (IsOn){
+			IsOn = false;
+			if (OnSwitchOff != null){
+				OnSwitchOff(this);
+			}
+		}else{
+			timeTillReset = Time.time + resetTime;
+			IsOn=true;
+			if (OnSwitchOn != null){
+				OnSwitchOn(this);
+			}
+		}
+	}
+}

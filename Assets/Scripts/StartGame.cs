5dfeaf298789f168d401b4074f704ea606a7520b
Thijs Verburg <t.verburg@codecast.nl>
2016-01-31 12:48:39 +0100

    added startscene

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+    public GameObject projectile;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Return"))
+        {
+
+        }
+    }
+}

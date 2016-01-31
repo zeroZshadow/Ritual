8616e668ba22fbf5abaa3b4f28f1bdeccac1cd4f
Mark Velthuis <da_heayray@hotmail.com>
2016-01-31 11:43:28 +0100

    add sound script

---
+++ Assets/Scripts/Reactors/PlaySoundOnSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnSwitch : MonoBehaviour {
+
+	public ToggleSwitch toggleSwitch;
+
+	// Use this for initialization
+	void Start () {
+		if (toggleSwitch == null){
+			toggleSwitch = GetComponent<ToggleSwitch>();
+		}
+
+		toggleSwitch.OnSwitchOn += HandleSwitchOn;
+		toggleSwitch.OnSwitchOff += HandleSwitchOff;
+	}
+	
+	void HandleSwitchOn(ToggleSwitch toggleSwitch){
+
+	}
+
+	void HandleSwitchOff(ToggleSwitch toggleSwitch){
+
+	}
+
+}

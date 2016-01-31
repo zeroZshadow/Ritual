2a287d88774d16aa064270a81067bd0a53b518ce
zeroZshadow <martijn_gerkes@hotmail.com>
2016-01-31 13:55:26 +0100

    Change camera distance

--- Assets/Scripts/PixelCamera.cs
+++ Assets/Scripts/PixelCamera.cs
@@ -2,18 +2,11 @@
 using System.Collections;
 
 public class PixelCamera : MonoBehaviour {
-
-	//Orthographic size = ((Vert Resolution)/(PPUScale * PPU)) * 0.5
-
 	public uint PPUScale = 1;
 	public uint PPU = 50;
-
-	public Transform target;
 	
 	// Update is called once per frame
 	void Update () {
 		Camera.main.orthographicSize = ((Screen.height) / (float)(PPUScale * PPU)) * 0.5f;
-
-		//TODO Move with target
 	}
 }
